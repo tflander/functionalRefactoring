7d884647ee3c62709c1e515ece0bd70179ee04cf
Todd Flanders <toddflanders@Todds-MacBook-Pro.local>
2018-08-17 15:56:20 -0400

    use interface for utility class

--- src/main/java/strategy/AssetUtil.java
+++ src/main/java/strategy/AssetUtil.java
@@ -2,9 +2,9 @@ package strategy;
 
 import java.util.List;
 
-public class AssetUtil {
+public interface AssetUtil {
 
-    public static int totalAssetValues(final List<Asset> assets) {
+    static int totalAssetValues(final List<Asset> assets) {
         return assets.stream()
                 .mapToInt(Asset::getValue)
                 .sum();
