ef7482a2a88e73436d0456edfbcfaae576331fdd
Todd Flanders <toddflanders@Todds-MacBook-Pro.local>
2018-09-10 17:32:47 -0400

    add hints in tests

--- src/test/java/executeAroundMethod/exercise/EmbeddedDeviceTest.java
+++ src/test/java/executeAroundMethod/exercise/EmbeddedDeviceTest.java
@@ -35,4 +35,38 @@ public class EmbeddedDeviceTest {
             }
         }
     }
+
+    @Test
+    public void hintOne() {
+        Runnable displayHintText = showCurryingHint("Hint: Use currying to construct functions");
+        displayHintText.run();
+    }
+
+    @Test
+    public void hintTwo() {
+        System.out.println("Functions, unlike methods, cannot throw checked exceptions");
+
+        EmbeddedDevice device = new EmbeddedDevice();
+
+        // Compile error: Unhandled Exception
+        // Runnable command = () -> device.doThis(10);
+
+        // ...but this is OK:
+         Runnable command = () -> {
+             try {
+                 device.doThis(10);
+             } catch (Exception e) {
+                 throw new IllegalStateException(e);
+             }
+         };
+
+    }
+
+    private Runnable showCurryingHint(String hint) {
+        return () -> {
+            System.out.println(hint);
+            System.out.println("This is an example of constructing a curried function that reduces the parameters from one to zero");
+            System.out.println("We call a function that takes one parameter and return a function that takes zero parameters");
+        };
+    }
 }
