b13cb9a6ae7a78ce0c516d4fd2570d607ba2244e
Todd Flanders <toddflanders@Todds-MacBook-Pro.local>
2018-09-10 16:30:05 -0400

    added commented out code to implement

--- src/test/java/executeAroundMethod/exercise/EmbeddedDeviceTest.java
+++ src/test/java/executeAroundMethod/exercise/EmbeddedDeviceTest.java
@@ -6,10 +6,18 @@ public class EmbeddedDeviceTest {
 
     @Test
     public void runDevice() throws Exception {
+
         EmbeddedDevice device = new EmbeddedDevice();
         device.powerUp();
         device.doThis(120);
         device.doThat(-45);
         device.powerDown();
+
+        // TODO: replace the block above with this commented out block
+        //        EmbeddedDevice.runCommands(
+//                EmbeddedDevice.doThisWith(120),
+//                EmbeddedDevice.doThatWith(-45)
+//        );
+
     }
 }
