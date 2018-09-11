b76e58437ac374a5c47517a711af4a07a8ef43b1
Todd Flanders <toddflanders@Todds-MacBook-Pro.local>
2018-09-11 16:18:46 -0400

    make item factory a little less klugy

--- src/main/java/factory/example/ItemFactory.java
+++ src/main/java/factory/example/ItemFactory.java
@@ -1,18 +1,16 @@
 package factory.example;
 
 public class ItemFactory {
+
     public static AbstractItem create(String itemName, int sellIn, int quality) {
 
         if(itemName.equals("Conjured")) {
             return new ConjuredAbstractItem(itemName, sellIn, quality);
-        }
-        if(itemName.equals("Backstage passes to a TAFKAL80ETC concert")) {
+        } else if(itemName.equals("Backstage passes to a TAFKAL80ETC concert")) {
             return new ConcertTicket(itemName, sellIn, quality);
-        }
-        if(itemName.equals("Sulfuras, Hand of Ragnaros")) {
+        } else if(itemName.equals("Sulfuras, Hand of Ragnaros")) {
             return new LegendaryAbstractItem(itemName, sellIn, quality);
-        }
-        if(itemName.equals("Aged Brie")) {
+        } else if(itemName.equals("Aged Brie")) {
             return new Brie(itemName, sellIn, quality);
         }
         return new NormalAbstractItem(itemName, sellIn, quality);
